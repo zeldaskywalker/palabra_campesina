d2cd81218840ac31d94b36864ba3e6880fad9b87
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2024-12-14 10:23:39 -0500

    Update settings.py

--- pc_site/settings.py
+++ pc_site/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
-                          "127.0.0.1,localhost,clownfish-app-4b869.ondigitalocean.app,palabracampesina.com").split(",")
+                          "127.0.0.1,localhost,https://clownfish-app-4b869.ondigitalocean.app/,palabracampesina.com").split(",")
 
 
 # Application definition
