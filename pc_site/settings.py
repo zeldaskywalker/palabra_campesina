1b86429f4bb990108ddb7782940d4c04011358a7
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-07-20 13:04:44 -0400

    small updates
    margin, nav bar color, logo size

--- pc_site/settings.py
+++ pc_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -29,7 +29,11 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
-                          "127.0.0.1,localhost,https://clownfish-app-4b869.ondigitalocean.app/,palabracampesina.com").split(",")
+                          "127.0.0.1," \
+                          "localhost," \
+                          "https://clownfish-app-4b869.ondigitalocean.app/," \
+                          "palabracampesina.com" \
+                          "www.palabracampesina.com").split(",")
 
 
 # Application definition
