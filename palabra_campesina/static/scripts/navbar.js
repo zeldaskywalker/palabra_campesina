bd13fba4cdcefd3e9937f61d850920b596543212
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-08-08 10:55:08 -0400

    dropdown navigation and gallery edits
    this change creates a dropdown navigation menu, which is meant for mobile. as such, with a certain screen size, the menu will collapse and become vertical rather than horizontal.
    
    gallery images were also updated with lower sized images to help with loading. the font sizes were adjusted in the gallery and singular gallery image pages, in order to match the rest of the sites font size changes.

--- palabra_campesina/static/scripts/navbar.js
+++ palabra_campesina/static/scripts/navbar.js
@@ -1,2 +1,44 @@
-console.log('nav bar logic will go here');
 // if screen width a certain size, show nav bar menu dropdown instead
+
+function openOrCloseNav() {
+    const navbar_items = document.getElementById("nav-items");
+
+    if (navbar_items.style.display === 'none') {
+        navbar_items.style.display = "flex";
+        navbar_items.style['flex-direction'] = 'column';
+    } else {
+        navbar_items.style.display = 'none';
+    }
+}
+
+const navbar_dropdown_button = document.getElementById('nav-button');
+navbar_dropdown_button.addEventListener('click', openOrCloseNav);
+
+function handleMobileOrResize() {
+  const navbar_items = document.getElementById("nav-items");
+  const navbar_dropdown_button = document.getElementById("nav-button");
+
+  if (window.innerWidth < 680) {
+    // if the screen IS NOT wide enough
+
+    // show the navbar dropdown button
+    navbar_dropdown_button.style.display = 'flex';
+
+    // hide the navbar items
+    navbar_items.style.display = 'none';
+
+  } else if (window.innerWidth >= 680) {
+    // if the screen IS wide enough
+
+    // don't show navbar dropdown button
+    navbar_dropdown_button.style.display = 'none';
+
+    // show navbar items horizontally
+    navbar_items.style.display = 'flex';
+    navbar_items.style['flex-direction'] = 'row';
+  }
+}
+
+handleMobileOrResize();
+
+window.onresize = handleMobileOrResize;
