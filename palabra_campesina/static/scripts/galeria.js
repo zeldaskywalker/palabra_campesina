e4965e478a53e9a79d07b9190059a941ab698702
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-08-12 09:37:40 -0400

    add placeholder background to images

--- palabra_campesina/static/scripts/galeria.js
+++ palabra_campesina/static/scripts/galeria.js
@@ -3,7 +3,18 @@ const gallery_items_container = document.getElementById('gallery_items');
 
 setTimeout(() => {
     if (loading_container) { // Check if the element still exists before attempting to remove
-        loading_container.remove();
+        loading_container.style.display = 'none';
         gallery_items_container.style.display = 'block';
     }
 }, 2000);
+
+const solo_galeria_img = document.getElementById('solo_galeria_image');
+// solo_galeria_img.src = solo_galeria_img.dataset.src;
+
+solo_galeria_img.onload = function() {
+    solo_galeria_img.style.visibility = 'visible';
+};
+
+if (!solo_galeria_img.complete) {
+    solo_galeria_img.style.visibility = 'hidden';
+}
