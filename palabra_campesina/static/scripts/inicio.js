1dbde44184e0cf78d37c844d32dcacfd66f1dc86
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-12-23 09:33:10 -0500

    Update inicio.js

--- palabra_campesina/static/scripts/inicio.js
+++ palabra_campesina/static/scripts/inicio.js
@@ -1,12 +1,13 @@
 function revealImageWithText() {
     const img_desktop_container = document.querySelector('.img_desktop_container');
     img_desktop_container.style.backgroundImage = imgDesktopTextUrl;
-    
+    console.log('text reveal');
 }
 
 function defaultHomeImageNoText() {
     const img_desktop_container = document.querySelector('.img_desktop_container');
     img_desktop_container.style.backgroundImage = imgDesktopNoTextUrl;
+    console.log('text hide');
 }
 
 const img_desktop_container = document.querySelector('.img_desktop_container');
