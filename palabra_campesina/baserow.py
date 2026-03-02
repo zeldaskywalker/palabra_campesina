ad1a3d9a5423a398722d6d4ae7fdb92d1f8163cf
Zelda Montes <zelda@montes.earth>
2026-03-02 13:42:31 -0500

    Update baserow.py
    Remove Authorization Token from Baserow database.

--- palabra_campesina/baserow.py
+++ palabra_campesina/baserow.py
@@ -1,12 +1,11 @@
-from django.urls import path
-import json
 import requests
-
 import os
 
+# TODO: UPDATE AUTHORIZATION TOKEN FOR EACH METHOD
+
+
 def get_stories_all():
-    # AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
-    AUTHORIZATION_TOKEN = 'mm7iccB819ZEZwnmTadyksgCrz5HfZvc'
+    AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
     AUTH_STRING = "Token " + AUTHORIZATION_TOKEN
     data = requests.get(
         "https://api.baserow.io/api/database/rows/table/392019/?user_field_names=true",
@@ -17,8 +16,7 @@ def get_stories_all():
     return data["results"]
 
 def get_story(row_id):
-    AUTHORIZATION_TOKEN = 'mm7iccB819ZEZwnmTadyksgCrz5HfZvc'
-    # AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
+    AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
     AUTH_STRING = "Token " + AUTHORIZATION_TOKEN
     request_string = "https://api.baserow.io/api/database/rows/table/392019/" + str(row_id) + "/?user_field_names=true"
     data = requests.get(
@@ -31,8 +29,7 @@ def get_story(row_id):
 
 
 def get_gallery_all_images():
-    AUTHORIZATION_TOKEN = 'mm7iccB819ZEZwnmTadyksgCrz5HfZvc'
-    # AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
+    AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
     AUTH_STRING = "Token " + AUTHORIZATION_TOKEN
     data = requests.get(
         "https://api.baserow.io/api/database/rows/table/392021/?user_field_names=true",
@@ -43,8 +40,7 @@ def get_gallery_all_images():
     return data["results"]
 
 def get_gallery_image_singular(row_id):
-    AUTHORIZATION_TOKEN = 'mm7iccB819ZEZwnmTadyksgCrz5HfZvc'
-    # AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
+    AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN")
     AUTH_STRING = "Token " + AUTHORIZATION_TOKEN
     request_string = "https://api.baserow.io/api/database/rows/table/392021/" + str(row_id) + "/?user_field_names=true"
     data = requests.get(
